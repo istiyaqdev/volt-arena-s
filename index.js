9efe4b505556e264570bf533753be43b353bef8d
Niaz-Tan <nuddintanim@gmail.com>
2026-05-31 23:27:58 +0600

    code improved

--- index.js
+++ index.js
@@ -22,15 +22,33 @@ const client = new MongoClient(uri, {
   },
 });
 
+
+
+
 const JWKS = createRemoteJWKSet(
   new URL(`${process.env.CLIENT_URL}/api/auth/jwks`),
 );
+
+
+
 const verifyToken = async (req, res, next) => {
   const authHeader = req?.headers.authorization;
   if (!authHeader) {
     return res.status(401).json({ message: "Unauthorized" });
   }
 
+
+
+
+
+
+
+
+
+
+
+
+  
   const token = authHeader.split(" ")[1];
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
