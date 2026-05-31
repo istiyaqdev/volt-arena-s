b71c31ed06fdb1a56f22785a36837f6996d36721
Niaz-Tan <nuddintanim@gmail.com>
2026-05-31 22:45:28 +0600

    logic improved

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/facility/:id", verifyToken, async (req, res) => {
+    app.get("/facility/:id", async (req, res) => {
       const id = req.params.id;
       const query = {
         _id: new ObjectId(id),
